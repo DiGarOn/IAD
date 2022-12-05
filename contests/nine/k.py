8740f3bc95d4afaf3e268a9b81c53a28c24f9845
Dmitriy Garkin <Dmitriy.Garkin@infowatch.com>
2022-12-05 17:19:39 +0300

    +k in 9

---
+++ contests/nine/k.py
@@ -0,0 +1,14 @@
+n, k= map(int, input().split())
+
+s = set()
+
+for i in range(k):
+    a,b = map(int,input().split())
+
+    if(a in s):
+        continue
+
+    s1 = {i for i in range(a, n+1, b) if not(i%7 in (0,6))}
+    s.update(s1)
+
+print(len(s))
